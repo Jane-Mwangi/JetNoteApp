53342a43d4f9050107d5b1b20c513b42d525a299
Jane Mwangi <janiegrace@gmail.com>
2023-05-15 06:44:00 +0300

    Add Providers in AppModule Class.

--- app/src/main/java/com/example/noteapp/di/AppModule.kt
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -1,12 +1,32 @@
 package com.example.noteapp.di
 
+import Note
+import android.content.Context
+import androidx.room.Room
+import com.example.noteapp.data.NoteDatabase
+import com.example.noteapp.data.NoteDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase): NoteDatabaseDao = noteDatabase.noteDao()
 
-
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): NoteDatabase =
+        Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "notes_db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
 }
